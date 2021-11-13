d397211b032cd76858cae0a8534bad51b2d24571
PPMike <74674465+PPMike@users.noreply.github.com>
2021-11-12 23:06:00 -0600

    Add files via upload

---
+++ Reflexion.cpp
@@ -0,0 +1,19 @@
+//A01383155
+//José Miguel Flores Glz
+
+/*
+
+Reflexión:
+
+El Código plantea el uso de grafos que básicamente en el uso 
+te das cuenta que es como usar listas, en este caso sería el uso de una lista 
+doblemente enlazada, también fue entretenido tener que repasar el uso de clases
+y de archivos .h (pero en este caso todos los implementamos como .cpp) y 
+su implementación en el main. 
+
+Tambien tuve que volver a revisar la implementación de las listas, al 
+principio fue dificil entender como empezar el programa pero luego se 
+fue haciendo mas sencillo durante la lluvia de ideas que realice con mi
+compañero Andres Ordieres con matricula A01382904.
+
+*/
